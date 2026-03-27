a4664f59c16c5c04044f6878950241ec56659a29
cpx <2900418703@qq.com>
2026-03-27 14:06:51 +0800

    Add Polaris CTF schedule

--- src/data/ctf-events.ts
+++ src/data/ctf-events.ts
@@ -12,6 +12,12 @@ export const CTF_EVENTS_EDIT_URL =
   'https://github.com/cpx1210/cpx1210.github.io/edit/main/src/data/ctf-events.ts';
 
 export const ctfEvents: CtfEvent[] = [
+  {
+    title: '第一届 Polaris CTF',
+    startDate: '2026-03-28T09:00:00+08:00',
+    endDate: '2026-03-30T09:00:00+08:00',
+    note: '北京时间',
+  },
   // Add new events here.
   // Example:
   // {
