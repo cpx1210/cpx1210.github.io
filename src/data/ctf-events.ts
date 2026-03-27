b73cae5ed8f869b7abc363491594371d86903fe0
cpx <2900418703@qq.com>
2026-03-27 14:00:10 +0800

    Add CTF schedule sidebar

---
+++ src/data/ctf-events.ts
@@ -0,0 +1,25 @@
+export interface CtfEvent {
+  title: string;
+  startDate: string;
+  endDate?: string;
+  platform?: string;
+  url?: string;
+  note?: string;
+}
+
+export const CTF_EVENTS_FILE_PATH = 'src/data/ctf-events.ts';
+export const CTF_EVENTS_EDIT_URL =
+  'https://github.com/cpx1210/cpx1210.github.io/edit/main/src/data/ctf-events.ts';
+
+export const ctfEvents: CtfEvent[] = [
+  // Add new events here.
+  // Example:
+  // {
+  //   title: 'ExampleCTF 2026',
+  //   startDate: '2026-04-12T09:00:00+08:00',
+  //   endDate: '2026-04-13T21:00:00+08:00',
+  //   platform: 'ctftime',
+  //   url: 'https://ctftime.org/',
+  //   note: 'Team event',
+  // },
+];
